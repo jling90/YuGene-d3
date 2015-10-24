a7c1bd6332ef80f61cd00d27f52b8499b62af4b6
root <root@ip-172-31-8-179.ap-southeast-2.compute.internal>
2015-10-24 08:32:14 +0000

    Fixed: renamed var filter -> cf to avoid naming conflict

--- javascripts/yugene-d3.js
+++ javascripts/yugene-d3.js
@@ -26,16 +26,16 @@ var chart = dc.barChart(".container")
 
 data_method(data_path, function(data){
     // Build a CrossFilter from our data
-    var filter = crossfilter(data)
+    var cf = crossfilter(data)
 
     // Define out data dimensions
-    var ugene_dim = filter.dimension(function(d){return +d.yugene_value})
-      , data_dim = filter.dimension(function(d){return +d.dataset_id})
-      , author_dim = filter.dimension(function(d){return d.dataset_name.split("_")[0]})
-      , year_dim = filter.dimension(function(d){return +d.dataset_name.split("_")[1]})
+    var ugene_dim = cf.dimension(function(d){return +d.yugene_value})
+      , data_dim = cf.dimension(function(d){return +d.dataset_id})
+      , author_dim = cf.dimension(function(d){return d.dataset_name.split("_")[0]})
+      , year_dim = cf.dimension(function(d){return +d.dataset_name.split("_")[1]})
 
     // Define data groups
-    var all = filter.groupAll()
+    var all = cf.groupAll()
       , data_by_u_val = ugene_dim.group()
       , data_by_id = data_dim.group()
       , data_by_author = author_dim.group()
