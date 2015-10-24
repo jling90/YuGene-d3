13679857d7646d53231e2ee9faf62ba01a380e0e
Tervel <jordananslow@gmail.com>
2015-10-24 19:51:38 +1000

    Fixed bootstrap-slider bug

--- javascripts/yugene-d3.js
+++ javascripts/yugene-d3.js
@@ -12,11 +12,6 @@ var mySlider = $("#ex1").slider();
 // Call a method on the slider
 var value = mySlider.slider('getValue');
 
-// For non-getter methods, you can chain together commands
-    mySlider
-        .slider('setValue', 5)
-        .slider('setValue', 7);
-
 var body = d3.select('body')
   , data_path = "yugene_gapdh_mouse.tsv"
   , ext = data_path.split('.')[data_path.split('.').length - 1]
